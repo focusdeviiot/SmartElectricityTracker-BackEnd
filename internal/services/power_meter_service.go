ea1fdba1df4281c149dea5e6a13886eee9c7fc13
Danuwat Baongoen (Focus) <81412150+focusdeviiot@users.noreply.github.com>
2024-06-06 01:27:09 +0700

    [UPDATE] read value

--- internal/services/power_meter_service.go
+++ internal/services/power_meter_service.go
@@ -39,76 +39,33 @@ func NewPowerMeterService(cfg *config.Config) (*PowerMeterService, error) {
 
 func (p *PowerMeterService) ReadAndStorePowerData() { //(broadcastFunc func(data interface{})) {
 	for {
-		// results, err := p.client.ReadInputRegisters(0, 16)
-		// if err != nil {
-		// 	log.Println(err)
-		// 	time.Sleep(5 * time.Second)
-		// 	continue
-		// }
 
-		readRegisters(p.client, 30001, "Voltage")
-		readRegisters(p.client, 30007, "Current")
-		readRegisters(p.client, 30013, "Active power")
-		readRegisters(p.client, 30019, "Apparent power")
-		readRegisters(p.client, 30025, "Reactive power")
-		readRegisters(p.client, 30031, "Power factor")
-		readRegisters(p.client, 30071, "Frequency")
-		readRegisters(p.client, 30073, "Import active energy")
-		readRegisters(p.client, 30075, "Export active energy")
-		readRegisters(p.client, 30077, "Import reactive energy")
-		readRegisters(p.client, 30079, "Export reactive energy")
+		address := uint16(30001)
+		quantity := uint16(18) // Read all registers from 30001 to 30080 (40 registers)
 
-		// voltage := Float32FromBytes(results[0:4])
-		// current := Float32FromBytes(results[6:10])
-		// power := Float32FromBytes(results[12:16])
-		// kwh := Float32FromBytes(results[18:22])
-		// kwg := Float32FromBytes(results[25:29])
+		results, err := p.client.ReadInputRegisters(address-30001, quantity)
+		if err != nil {
+			logf.Infof("Error reading registers: %v\n", err)
+			return
+		}
 
-		// err = p.usageRepository.StoreElectricityUsage(voltage, current, power, 1) // 1: สมมติว่าเป็น user ID
-		// if err != nil {
-		// 	log.Println(err)
-		// }
-
-		// data := map[string]float32{
-		// 	"voltage": voltage,
-		// 	"current": current,
-		// 	"power":   power,
-		// }
-
-		// Call the broadcast function
-		// broadcastFunc(data)
-
-		// logf.Info("Voltage: ", voltage)
-		// logf.Info("Current: ", current)
-		// logf.Info("Power: ", power)
-		// logf.Info("kWh: ", kwh)
-		// logf.Info("kWg: ", kwg)
+		values := parseRegisters(results)
+		logf.Infof("Voltage: %f\n", values[0])
+		logf.Infof("Current: %f\n", values[1])
+		logf.Infof("Active power: %f\n", values[2])
 
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func readRegisters(client modbus.Client, address uint16, paramName string) {
-	quantity := uint16(2) // Each float is 4 bytes, which is 2 registers
-	results, err := client.ReadInputRegisters(address-30001, quantity)
-	if err != nil {
-		logf.Infof("Error reading %s: %v\n", paramName, err)
-		return
+func parseRegisters(results []byte) []float32 {
+	values := make([]float32, 3)
+	for i := 0; i < 3; i++ {
+		start := i * 12
+		end := start + 4
+		values[i] = Float32FromBytes(results[start:end])
 	}
-
-	if len(results) != 4 {
-		logf.Infof("Unexpected length of results for %s: %v\n", paramName, results)
-		return
-	}
-
-	// Convert bytes to float32
-	value := Float32FromBytes(results)
-	logf.Infof("%s: %f\n", paramName, value)
-}
-
-func float32frombytes(bytes []byte) float32 {
-	bits := uint32(bytes[3]) | uint32(bytes[2])<<8 | uint32(bytes[1])<<16 | uint32(bytes[0])<<24
-	return float32(bits)
+	return values
 }
 
 func Float32FromBytes(bytes []byte) float32 {
